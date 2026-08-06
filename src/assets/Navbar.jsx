8e8a1582a1ea31885f51b981523c5033128eaf30
Pavithran S <pavithrans.cs25@bitsathy.ac.in>
2026-08-06 22:45:56 +0530

    added data

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav className="flex items-center gap-200 bg-black border-b border-slate-800 shadow-lg">
         <div className="flex items-center justify-between py-6 px-[45px]">
-            <h2 className="text-3xl font-bold text-white tracking-wide cursor-pointer hover:text-purple-400 transition-colors duration-300">🎮 Game Verse</h2>
+            <NavLink className="text-3xl font-bold text-white tracking-wide cursor-pointer hover:text-purple-400 transition-colors duration-300">🎮 Game Verse</NavLink>
         </div>
         <div className="flex items-center gap-6">
             <NavLink to="/"className={({isActive}) => isActive ? "bg-white text-black px-4 py-2 rounded-lg" : "text-white hover:bg-white hover:text-black px-4 py-2 rounded-lg cursor-pointer transition-colors duration-300"}>Home</NavLink>
