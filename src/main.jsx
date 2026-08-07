3e6939ba81d0f50babf9a51c4614d627da51d51f
Pavithran S <pavithrans.cs25@bitsathy.ac.in>
2026-08-07 14:19:06 +0530

    added wishlist meassage

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Game from './assets/Game'
 import Wishlist from './assets/Wishlist'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
+import { Toaster }from 'react-hot-toast'
 
 const router = createBrowserRouter([
     {
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Toaster position="top-center" />
     <RouterProvider router={ router }/>
   </StrictMode>
 )
