3643316e5baaf3669246b5b5eeb6917d4f29ea92
Pavithran S <pavithrans.cs25@bitsathy.ac.in>
2026-09-02 23:23:38 +0530

    wishlist adding successfull

--- src/assets/Wishlist.jsx
+++ src/assets/Wishlist.jsx
@@ -1,43 +1,59 @@
 import React from 'react'
 import Navbar from './Navbar'
+import React, { useState, useEffect } from 'react'
 
 function Wishlist() {
 
-  const game = JSON.parse(localStorage.getItem("wishlist"));
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    const game = JSON.parse(localStorage.getItem("wishlist")) || [];
+    setWishlist(game);
+  }, []);
 
   return (
     <div>
-      <Navbar/>
-       <div className="min-h-screen bg-slate-950 text-white p-10">
+      <Navbar />
 
-            <h1 className="text-4xl font-bold mb-8">
-                My Wishlist
-            </h1>
+      <div className="min-h-screen bg-slate-950 text-white p-10">
 
-            {game && (
-                <div className="bg-slate-900 p-5 rounded-xl w-80">
+        <h1 className="text-4xl font-bold mb-8">
+          My Wishlist
+        </h1>
 
-                    <img
-                        src={game.image}
-                        className="w-full h-48 object-cover rounded-xl"
-                    />
+        <div className="grid grid-cols-3 gap-6">
 
-                    <h2 className="text-2xl font-bold mt-4">
-                        {game.title}
-                    </h2>
+          {wishlist.map((game) => (
 
-                    <p className="text-gray-400 mt-2">
-                        {game.genre}
-                    </p>
+            <div
+              key={game.id}
+              className="bg-slate-900 p-5 rounded-xl"
+            >
 
-                    <p className="text-cyan-400 mt-2">
-                        ⭐ {game.rating}
-                    </p>
+              <img
+                src={game.image}
+                className="w-full h-48 object-cover rounded-xl"
+              />
 
-                </div>
-            )}
+              <h2 className="text-2xl font-bold mt-4">
+                {game.title}
+              </h2>
+
+              <p className="text-gray-400 mt-2">
+                {game.genre}
+              </p>
+
+              <p className="text-cyan-400 mt-2">
+                ⭐ {game.rating}
+              </p>
+
+            </div>
+
+          ))}
 
         </div>
+
+      </div>
     </div>
   )
 }
